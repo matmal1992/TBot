95f49e3e25f6df7a88cbafae3d9ff4a9080089b9
matmal1992 <mateusz.malek.programista@gmail.com>
2025-04-09 21:34:13 +0200

    Format all files properly

--- headers/BotSimulator.h
+++ headers/BotSimulator.h
@@ -4,36 +4,38 @@
 #include "LoadCsvData.h"
 #include <deque>
 
-struct Actions {
-  std::vector<bool> opens;
-  std::vector<bool> closes;
+struct Actions
+{
+    std::vector<bool> opens;
+    std::vector<bool> closes;
 };
-class BotSimulator {
+class BotSimulator
+{
 public:
-  BotSimulator(const std::vector<double> &prices);
+    BotSimulator(const std::vector<double>& prices);
 
-  void Iterate();
-  Actions GetActions();
+    void Iterate();
+    Actions GetActions();
 
 private:
-  void AddRecord(int record_index);
-  bool IsConstantFall();
-  bool IsConstantRise();
-  bool IsSuddenFall();
-  bool IsSuddenRise();
+    void AddRecord(int record_index);
+    bool IsConstantFall();
+    bool IsConstantRise();
+    bool IsSuddenFall();
+    bool IsSuddenRise();
 
-  std::vector<double> prices_;
-  std::deque<double> current_records;
-  size_t amount_of_current_records = 5;
-  double percentage_profit = 0.05;
-  double percentage_loss = 0.05;
-  double percentage_peak_profit = 1.2;
-  double percentage_peak_loss = 0.2;
-  size_t investing_value = 5000;
+    std::vector<double> prices_;
+    std::deque<double> current_records;
+    size_t amount_of_current_records = 5;
+    double percentage_profit = 0.05;
+    double percentage_loss = 0.05;
+    double percentage_peak_profit = 1.2;
+    double percentage_peak_loss = 0.2;
+    size_t investing_value = 5000;
 
-  double balance{0};
-  size_t buys{0}, sells{0};
-  Actions actions;
+    double balance {0};
+    size_t buys {0}, sells {0};
+    Actions actions;
 };
 
 #endif
--- src/LoadCsvData.cpp
+++ src/LoadCsvData.cpp
@@ -1,107 +1,118 @@
 #include "../headers/LoadCsvData.h"
 
-CsvData::CsvData(const std::string &path) : path_(path) { ; }
-
-LoadedData CsvData::LoadDataFromFile() {
-  LoadedData data;
-
-  std::ifstream file(path_);
-  if (!file) {
-    std::cerr << "Error: Unable to open file.\n";
-    return data; // to do: better error hadndling
-  }
-
-  std::string line;
-
-  // Skip the first line (header)
-  std::getline(file, line);
-
-  while (std::getline(file, line)) {
-    std::stringstream ss(line);
-    std::string date, priceStr;
-
-    std::getline(ss, date, ',');     // Read first column (date)
-    std::getline(ss, priceStr, ','); // Read second column (price)
-
-    double price = std::stod(priceStr);
-    data.prices.push_back(price);
-    data.times.push_back(date);
-    // to do: invalid data hadndling
-  }
-
-  file.close();
-  return data;
+CsvData::CsvData(const std::string& path)
+    : path_(path)
+{
+    ;
 }
 
-void CsvData::PrintGraph(const LoadedData &data,
-                         const std::vector<bool> &opens) {
-  // Write data to a temporary file for gnuplot
-  std::ofstream tempFile("plot_data.txt");
-  std::ofstream highlightFile("highlight_data.txt"); // New file for markers
+LoadedData CsvData::LoadDataFromFile()
+{
+    LoadedData data;
 
-  for (size_t i = 0; i < data.times.size(); ++i) {
-    tempFile << i << " " << data.prices[i]
-             << "\n"; // Use index instead of time for x-axis
-
-    // Save every 60th point for highlighting
-    if (opens.at(i) == true) {
-      highlightFile << i << " " << data.prices[i] << "\n";
+    std::ifstream file(path_);
+    if (!file)
+    {
+        std::cerr << "Error: Unable to open file.\n";
+        return data; // to do: better error hadndling
     }
-  }
-  tempFile.close();
-  highlightFile.close();
 
-  // Generate gnuplot command with markers
-  std::string command =
-      "gnuplot -e \""
-      "set title 'US500 Price'; "
-      "set grid; "
-      "plot 'plot_data.txt' using 1:2 with lines title 'Price', "
-      "'highlight_data.txt' using 1:2 with points pointtype 7 pointsize 1.5 lc "
-      "rgb 'green' title '60-min Markers'; "
-      "pause -1\"";
-  system(command.c_str());
-}
+    std::string line;
 
-double CsvData::GetAvgValue(const std::vector<double> &num_data) {
-  if (num_data.empty()) {
-    std::cerr << "No valid data found.\n";
-    return 0;
-  }
+    // Skip the first line (header)
+    std::getline(file, line);
 
-  double sum{0};
-  for (double num : num_data) {
-    sum += num;
-  }
+    while (std::getline(file, line))
+    {
+        std::stringstream ss(line);
+        std::string date, priceStr;
 
-  double average = sum / num_data.size();
-  return average;
-}
+        std::getline(ss, date, ','); // Read first column (date)
+        std::getline(ss, priceStr, ','); // Read second column (price)
 
-double CsvData::GetBiggestDifference(const std::vector<double> &num_data) {
-  if (num_data.empty()) {
-    std::cerr << "No valid data found.\n";
-    return 0;
-  }
-
-  double difference{0}, tmp_difference{0};
-
-  for (size_t i{1}; i < num_data.size(); ++i) {
-    double diff = num_data.at(i) > num_data.at(i - 1)
-                      ? num_data.at(i) - num_data.at(i - 1)
-                      : num_data.at(i - 1) - num_data.at(i);
-    if (std::abs(diff) > difference) {
-      difference = diff;
+        double price = std::stod(priceStr);
+        data.prices.push_back(price);
+        data.times.push_back(date);
+        // to do: invalid data hadndling
     }
-  }
 
-  return difference;
+    file.close();
+    return data;
 }
 
-DiagnosticData CsvData::GetDiagnosticData(const std::vector<double> &prices) {
-  DiagnosticData data;
-  data.avg_price = GetAvgValue(prices);
-  data.biggest_difference = GetBiggestDifference(prices);
+void CsvData::PrintGraph(const LoadedData& data, const std::vector<bool>& opens)
+{
+    // Write data to a temporary file for gnuplot
+    std::ofstream tempFile("plot_data.txt");
+    std::ofstream highlightFile("highlight_data.txt"); // New file for markers
 
-  return data;
+    for (size_t i = 0; i < data.times.size(); ++i)
+    {
+        tempFile << i << " " << data.prices[i] << "\n"; // Use index instead of time for x-axis
+
+        // Save every 60th point for highlighting
+        if (opens.at(i) == true)
+        {
+            highlightFile << i << " " << data.prices[i] << "\n";
+        }
+    }
+    tempFile.close();
+    highlightFile.close();
+
+    // Generate gnuplot command with markers
+    std::string command = "gnuplot -e \""
+                          "set title 'US500 Price'; "
+                          "set grid; "
+                          "plot 'plot_data.txt' using 1:2 with lines title 'Price', "
+                          "'highlight_data.txt' using 1:2 with points pointtype 7 pointsize 1.5 lc "
+                          "rgb 'green' title '60-min Markers'; "
+                          "pause -1\"";
+    system(command.c_str());
+}
+
+double CsvData::GetAvgValue(const std::vector<double>& num_data)
+{
+    if (num_data.empty())
+    {
+        std::cerr << "No valid data found.\n";
+        return 0;
+    }
+
+    double sum {0};
+    for (double num : num_data) { sum += num; }
+
+    double average = sum / num_data.size();
+    return average;
+}
+
+double CsvData::GetBiggestDifference(const std::vector<double>& num_data)
+{
+    if (num_data.empty())
+    {
+        std::cerr << "No valid data found.\n";
+        return 0;
+    }
+
+    double difference {0}, tmp_difference {0};
+
+    for (size_t i {1}; i < num_data.size(); ++i)
+    {
+        double diff = num_data.at(i) > num_data.at(i - 1) ? num_data.at(i) - num_data.at(i - 1)
+                                                          : num_data.at(i - 1) - num_data.at(i);
+        if (std::abs(diff) > difference)
+        {
+            difference = diff;
+        }
+    }
+
+    return difference;
+}
+
+DiagnosticData CsvData::GetDiagnosticData(const std::vector<double>& prices)
+{
+    DiagnosticData data;
+    data.avg_price = GetAvgValue(prices);
+    data.biggest_difference = GetBiggestDifference(prices);
+
+    return data;
 }
