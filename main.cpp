378dd0eb1bef51914aebb5d12c2419c39ede05d8
matmal92 <mateusz.malek@ncbj.gov.pl>
2025-04-13 12:47:17 +0200

    Add overall result of taken opens/closes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include "headers\BotSimulator.h"
 
+#include <algorithm>
+
 int main()
 {
     std::vector<double> test_prices {1, 2, 3, 4, 5, 6, 4, 5, 6, 7, 8, 9, 7, 8, 9, 10, 11, 9, 10, 11, 12, 13, 14};
+    std::reverse(test_prices.begin(), test_prices.end());
 
-    CsvData data("US500_prices.csv");
-    // CsvData data(test_prices);
+    // CsvData data("US500_prices.csv");
+    CsvData data(test_prices);
     // BotSimulator bot_simulator(test_prices);
     BotSimulator bot_simulator(data.GetPrices());
+    std::cout << "Balance: " << bot_simulator.GetBalance() << std::endl;
 
     data.PrintGraph(data.GetDiagnosticData(), bot_simulator.GetActions());
 
