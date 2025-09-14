50dc8db5d5eb141cbf3dc16677cd9b42091201ed
matmal1992 <mateusz.malek.programista@gmail.com>
2025-09-14 13:23:27 +0200

    Use inline constexpr for global consts

--- headers/Patterns.h
+++ headers/Patterns.h
@@ -7,10 +7,10 @@
 #include <functional>
 #include <vector>
 
-const double percentage_profit = 0.05;
-const double percentage_loss = 0.05;
-const double percentage_peak_profit = 1.9;
-const double percentage_peak_loss = 1.9;
+inline constexpr double percentage_profit = 0.05;
+inline constexpr double percentage_loss = 0.05;
+inline constexpr double percentage_peak_profit = 1.9;
+inline constexpr double percentage_peak_loss = 1.9;
 
 bool IsConstantFall(const std::deque<double>& current_records);
 bool IsConstantRise(const std::deque<double>& current_records);
