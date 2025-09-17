9ef76b58274a7a4092f39c151a5365505d1f533e
matmal1992 <mateusz.malek.programista@gmail.com>
2025-09-17 12:59:27 +0200

    Update and add more tests for ConstantTendency()

--- tests/PatternsTest.cpp
+++ tests/PatternsTest.cpp
@@ -1,53 +1,97 @@
 #include "../headers/Patterns.h"
 #include "gtest/gtest.h"
 
-TEST(CheckTendencyInRow, SingleRiseTrue)
+TEST(ConstantTendencyTest, SingleRiseTrueForDequeAndVector)
 {
-    std::deque<double> data = {1.0, 2.0};
-    EXPECT_TRUE(ConstantTendency(data, 1, std::greater<>()));
+    std::deque<double> d_data = {1.0, 2.0};
+    EXPECT_TRUE(ConstantTendency(d_data, 1, std::greater<>()));
+
+    d_data = {2.0, 1.0};
+    EXPECT_FALSE(ConstantTendency(d_data, 1, std::greater<>()));
+
+    std::vector<double> v_data = {1.9999, 2.0};
+    EXPECT_TRUE(ConstantTendency(v_data, 1, std::greater<>()));
 }
 
-TEST(CheckTendencyInRow, SingleRiseFalse)
+TEST(ConstantTendencyTest, SingleFallTrueForDequeAndVector)
 {
-    std::deque<double> data = {2.0, 1.0};
-    EXPECT_FALSE(ConstantTendency(data, 1, std::greater<>()));
+    std::deque<double> d_data = {2.0, 1.0};
+    EXPECT_TRUE(ConstantTendency(d_data, 1, std::less<>()));
+
+    d_data = {1.0, 2.0};
+    EXPECT_FALSE(ConstantTendency(d_data, 1, std::less<>()));
+
+    std::vector<double> v_data = {2.0, 1.9999};
+    EXPECT_TRUE(ConstantTendency(v_data, 1, std::less<>()));
 }
 
-TEST(CheckTendencyInRow, TwoFallsInRowTrue)
+TEST(ConstantTendencyTest, DoubleRiseTrue)
 {
-    std::deque<double> data = {5.0, 4.0, 3.0};
-    EXPECT_TRUE(ConstantTendency(data, 2, std::less<>()));
+    std::deque<double> d_data = {1.0, 2.0, 3.0};
+    EXPECT_TRUE(ConstantTendency(d_data, 2, std::greater<>()));
+
+    d_data = {1.0, 3.0, 2.0};
+    EXPECT_FALSE(ConstantTendency(d_data, 2, std::greater<>()));
 }
 
-TEST(CheckTendencyInRow, TwoFallsInRowFalse)
+TEST(ConstantTendencyTest, DoubleFallTrue)
 {
-    std::deque<double> data = {5.0, 4.0, 5.0};
-    EXPECT_FALSE(ConstantTendency(data, 2, std::less<>()));
+    std::deque<double> d_data = {3.0, 2.0, 1.0};
+    EXPECT_TRUE(ConstantTendency(d_data, 2, std::less<>()));
+
+    d_data = {3.0, 1.0, 2.0};
+    EXPECT_FALSE(ConstantTendency(d_data, 2, std::less<>()));
 }
 
-TEST(PatternsTests, SingleRise)
+TEST(ConstantTendencyTest, FiveTimesInRowFallTrue)
 {
-    std::deque<double> data = {1.0, 2.0};
-    EXPECT_TRUE(ConstantTendency(data, 1, std::greater<>()));
+    std::deque<double> d_data = {10.0, 8.0, 6.0, 4.0, 2.0, 0.0};
+    EXPECT_TRUE(ConstantTendency(d_data, 5, std::less<>()));
 
-    data = {2.0, 1.0};
-    EXPECT_FALSE(ConstantTendency(data, 1, std::greater<>()));
+    d_data = {10.0, 8.0, 6.0, 4.0, 2.0, 1.0}; // still strictly decreasing
+    EXPECT_TRUE(ConstantTendency(d_data, 5, std::less<>()));
+
+    d_data = {10.0, 8.0, 6.0, 4.0, 2.0, 3.0}; // last value breaks the fall
+    EXPECT_FALSE(ConstantTendency(d_data, 5, std::less<>()));
+
+    d_data = {10.0, 11.0, 6.0, 4.0, 2.0, 3.0}; // second value breaks the fall
+    EXPECT_FALSE(ConstantTendency(d_data, 5, std::less<>()));
 }
 
-TEST(PatternsTests, TwoRisesInRow)
+TEST(ConstantTendencyTest, FiveTimesInRowRiseTrue)
 {
-    std::deque<double> data = {1.0, 2.0, 3.0};
-    EXPECT_TRUE(ConstantTendency(data, 2, std::greater<>()));
+    std::deque<double> d_data = {0.0, 2.0, 4.0, 6.0, 8.0, 10.0};
+    EXPECT_TRUE(ConstantTendency(d_data, 5, std::greater<>()));
 
-    data = {1.0, 2.0, 1.0};
-    EXPECT_FALSE(ConstantTendency(data, 2, std::greater<>()));
+    d_data = {0.0, 2.0, 4.0, 6.0, 8.0, 9.0}; // still strictly increasing
+    EXPECT_TRUE(ConstantTendency(d_data, 5, std::greater<>()));
+
+    d_data = {0.0, 2.0, 4.0, 6.0, 8.0, 7.0}; // last value breaks the rise
+    EXPECT_FALSE(ConstantTendency(d_data, 5, std::greater<>()));
+
+    d_data = {1.0, 0.5, 4.0, 6.0, 8.0, 7.0}; // second value breaks the rise
+    EXPECT_FALSE(ConstantTendency(d_data, 5, std::greater<>()));
 }
 
-TEST(PatternsTests, ShortAvgDoubleDecrease)
+TEST(ConstantTendencyTest, EqualValuesInRow)
 {
-    std::vector<double> short_avg = {5.0, 4.0, 3.0};
-    EXPECT_TRUE(ConstantTendency(short_avg, 0, std::less<>()));
+    std::deque<double> d_data = {1.0, 1.0, 3.0};
+    EXPECT_FALSE(ConstantTendency(d_data, 2, std::greater<>()));
 
-    short_avg = {5.0, 5.0, 3.0};
-    EXPECT_FALSE(ConstantTendency(short_avg, 0, std::less<>()));
+    d_data = {1.0, 1.0, 0.5};
+    EXPECT_FALSE(ConstantTendency(d_data, 2, std::less<>()));
+}
+
+TEST(ConstantTendencyTest, SingleValueInContainer)
+{
+    std::deque<double> d_data = {1.0};
+    EXPECT_FALSE(ConstantTendency(d_data, 1, std::greater<>()));
+    EXPECT_FALSE(ConstantTendency(d_data, 1, std::less<>()));
+}
+
+TEST(ConstantTendencyTest, EmptyContainer)
+{
+    std::deque<double> d_data = {};
+    EXPECT_FALSE(ConstantTendency(d_data, 1, std::greater<>()));
+    EXPECT_FALSE(ConstantTendency(d_data, 1, std::less<>()));
 }
